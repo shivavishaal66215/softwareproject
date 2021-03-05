1ccd4ae25e4ff5a682e8d00a5fdaf50bea082044
Vishaal S <shivavishaal25@gmail.com>
2021-03-05 12:34:09 +0530

    with animation on faculty

--- site/js/app.js
+++ site/js/app.js
@@ -1,25 +1,50 @@
-Barba.Pjax.start();
+window.addEventListener("load", function() {
+    var cont = document.getElementById("welcome");
 
+    var cont3 = document.getElementById("welcometext");
 
-var FadeTransition = Barba.BaseTransition.extend({
-    start: function() {
-        /**
-         * This function is automatically called as soon the Transition starts
-         * this.newContainerLoading is a Promise for the loading of the new container
-         * (Barba.js also comes with an handy Promise polyfill!)
-         */
-        // As soon the loading is finished and the old page is faded out, let's fade the new page
-        Promise
-            .all([this.newContainerLoading, this.fadeOut()])
-            .then(this.fadeIn.bind(this));
-    },
-    fadeOut: function() {},
-    fadeIn: function() {
-        this.newContainer.classList.add("slide-in");
+    cont.classList.add('fadein');
+    var timer3 = setInterval(movef, 50);
+    var counter3 = 0;
+
+    function movef() {
+        counter3++;
+        if (counter3 == 20) {
+            clearInterval(timer3);
+            timer3 = null;
+            return;
+        }
     }
+
+
+
+    cont3.classList.add('moveright');
+    var timer = setInterval(mover, 50);
+    var counter = 0;
+
+
+    function mover() {
+
+        counter++;
+        if (counter == 20) {
+            clearInterval(timer);
+            timer = null;
+            return;
+        }
+    }
+
+    var cont2 = document.getElementById("login-container");
+    cont2.classList.add('moveleft');
+    var timer2 = setInterval(movel, 50);
+    var counter2 = 0;
+
+    function movel() {
+        counter2++;
+        if (counter2 == 20) {
+            clearInterval(timer2);
+            timer2 = null;
+            return;
+        }
+    }
+
 });
-
-Barba.Pjax.getTransition = function() {
-
-    return FadeTransition;
-};
